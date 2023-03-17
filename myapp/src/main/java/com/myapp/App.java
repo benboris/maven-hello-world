6d1eb9ef6859ee2778fc8e2ae9b633e57ea36182
benboris <41168284+benboris@users.noreply.github.com>
2023-03-17 22:44:11 +0200

    Update App.java
    Add my name to script

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        c "Hello World! +"\n" My Name is Ben Kandel " );
     }
 }
