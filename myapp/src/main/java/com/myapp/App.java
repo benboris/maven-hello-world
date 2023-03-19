f40a0a4a91b77165b9ff2c481ac5b551a97c4976
benboris <41168284+benboris@users.noreply.github.com>
2023-03-19 17:50:33 +0200

    Update App.java

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        c "Hello World! +"\n" My Name is Ben Kandel " );
+        c "Hello World! + "\n" My Name is Ben Kandel " );
     }
 }
